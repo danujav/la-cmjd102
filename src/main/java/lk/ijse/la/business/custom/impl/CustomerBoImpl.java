7a67138c335872fb3bd0cc849f3fe1475261fd1b
danu <danujagreru@gmail.com>
2023-10-14 16:08:16 +0530

    pass bo to dao

--- src/main/java/lk/ijse/la/business/custom/impl/CustomerBoImpl.java
+++ src/main/java/lk/ijse/la/business/custom/impl/CustomerBoImpl.java
@@ -1,11 +1,23 @@
 package lk.ijse.la.business.custom.impl;
 
 import lk.ijse.la.business.custom.CustomerBo;
+import lk.ijse.la.dao.CustomerDao;
 import lk.ijse.la.dto.CustomerDto;
+import lk.ijse.la.entity.Customer;
 
 public class CustomerBoImpl implements CustomerBo {
     @Override
     public boolean saveCustomer(CustomerDto dto) throws Exception {
-        return false;
+        String id = dto.getId();
+        String name = dto.getName();
+        String address = dto.getAddress();
+        String tel = dto.getTel();
+
+        var customer = new Customer(id, name, address, tel);
+
+        CustomerDao customerDao = new CustomerDao();
+        boolean isSaved = customerDao.save(customer);
+
+        return isSaved;
     }
 }
---
+++ src/main/java/lk/ijse/la/dao/CustomerDao.java
@@ -0,0 +1,9 @@
+package lk.ijse.la.dao;
+
+import lk.ijse.la.entity.Customer;
+
+public class CustomerDao {
+    public boolean save(Customer customer) {
+        return false;
+    }
+}
