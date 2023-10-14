69fb7f8ba1fe9fc3ae708fb20b19a591b83e51bd
danu <danujagreru@gmail.com>
2023-10-14 15:10:01 +0530

    initial commit

---
+++ src/main/java/lk/ijse/la/controller/CustomerFormController.java
@@ -0,0 +1,27 @@
+package lk.ijse.la.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CustomerFormController {
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtTel;
+
+    @FXML
+    void i(ActionEvent event) {
+
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+}
